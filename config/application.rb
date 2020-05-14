a8896f8db3023ee7c6a3bab7aa1f5d8e23af0c4e
toshi-ue <toshihiro.u.0207@gmail.com>
2020-05-15 07:18:39 +0900

    update: i18nの設定(日本語化)を追加

--- config/application.rb
+++ config/application.rb
@@ -21,9 +21,10 @@ module Webapp
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
 
-    # Settings in config/environments/* take precedence over those specified here.
-    # Application configuration should go into files in config/initializers
-    # -- all .rb files in that directory are automatically loaded.
+    # locale
+    config.i18n.available_locales = [ :en, :ja ]
+    # default locale
+    config.i18n.default_locale = :ja
 
     # Don't generate system test files.
     config.generators.system_tests = nil
