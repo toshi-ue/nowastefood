02cdc07f905dc23b21c72d71eaa36348b40c8f86
toshi-ue <toshihiro.u.0207@gmail.com>
2020-05-26 05:34:59 +0900

    add: foodcategoryのモデルスペックを追加 #39

--- spec/factories/managers.rb
+++ spec/factories/managers.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :manager do
-    
+    sequence(:email) { |n| "manager#{n}@gmail.com" }
+    password { 'password1234' }
+    password_confirmation { 'password1234' }
+    confirmed_at { Date.today }
   end
 end
