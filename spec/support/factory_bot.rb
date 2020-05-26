047ea93966b0dd17a81fa9cf665deda1482f6b46
toshi-ue <toshihiro.u.0207@gmail.com>
2020-05-26 12:14:07 +0900

    update: RSpecの必要な設定を修正

---
+++ spec/support/factory_bot.rb
@@ -0,0 +1,10 @@
+RSpec.configure do |config|
+  # FactoryBotの呼び出し簡略化
+  #   テスト中に、FactoryBot.create(:user) ではなく、create(:user) と書けるようになる。
+  config.include FactoryBot::Syntax::Methods
+
+  # springが原因でfactoryが正しく読み込まれないことを防ぐ
+  config.before :all do
+    FactoryBot.reload
+  end
+end
