5a355ca60a8df5f68d4d24f77c794364860e6e59
toshi-ue <toshihiro.u.0207@gmail.com>
2020-06-06 06:10:20 +0900

    change: cookedstatesモデルのname属性にindexを付加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200605205103) do
+ActiveRecord::Schema.define(version: 20200605210754) do
 
   create_table "cookedstates", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name", comment: "加工された状態の名前"
