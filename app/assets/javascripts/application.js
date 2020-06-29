492271d876c59ecf2f61454f7c55761197a05a4c
belion-freee <belion.freee@gmail.com>
2020-06-29 11:42:12 +0900

    fix application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,9 +1,10 @@
-//= require rails-ujs/lib/assets/compiled/rails-ujs.js
 //= require jquery/dist/jquery.js
-//= require popper.js/dist/popper.js
+//= require rails-ujs/lib/assets/compiled/rails-ujs.js
+//= require jquery-ui/ui/widget.js
+//= require popper.js/dist/umd/popper.min.js
 //= require bootstrap/dist/js/bootstrap.js
-//= require jquery-ui/ui/widgets/sortable.js
 //= require clipboard/dist/clipboard.min.js
+//= require @fortawesome/fontawesome-free/js/all.js
 
 // TODO: jQueryの動作確認がとれない1
 $(function () { // if document is ready
