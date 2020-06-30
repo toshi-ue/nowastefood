aee2f7a13e2e0c16a35269b4f442e6522b83a696
toshi-ue <toshihiro.u.0207@gmail.com>
2020-06-30 18:58:45 +0900

    wip12

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,38 +7,4 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
-Rails.application.config.assets.precompile += %w[
-  home.css
-  devise/confirmations.css
-  devise/unlocks.css
-  managers/*.js
-  managers/confirmations.css
-  managers/cookedstates.css
-  managers/cuisines.css
-  managers/cuisines.js
-  managers/foodcategories.css
-  managers/foodstuffs.css
-  managers/foodstuffs.js
-  managers/ingredients.css
-  managers/procedures.css
-  managers/rawmaterials.css
-  managers/registrations.css
-  managers/sessions.css
-  managers/sessions.js
-  users/cuisines.css
-  users/sessions.css
-  users/registrations.css
-]
-
-# TODO: 下2つの記述だと ActionView::Template::Error が発生する
-# Rails.application.config.assets.precompile += [
-#   '*.css',
-#   '*.js',
-#   'managers/*.css',
-#   'managers/*.js',
-#   'users/*.css',
-#   'users/*.js',
-#   'devise/*.css'
-# ]
-
-# Rails.application.config.assets.precompile += ['*.js', '*.css']
+Rails.application.config.assets.precompile += ['*.js', '*.css']
