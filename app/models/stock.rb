9a977aac804a50c751153fcca4320487284baab7
toshi-ue <toshihiro.u.0207@gmail.com>
2020-07-21 07:32:49 +0900

    update: stockモデルに独自のバリデーションを追加

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,7 @@ class Stock < ApplicationRecord
   belongs_to :rawmaterial
   belongs_to :user
   # quantityのvalidation
+  #  validationがokなら値を半角に変換して保存したい
   #   もとはstring
   #   変換する必要あり
   #   整数(大文字、小文字)ならok
@@ -18,18 +19,20 @@ class Stock < ApplicationRecord
   #   それ以外はng
   #   okであれば大文字の場合は小文字に変換
 
-  # validates :cannot_save_except_specific_format
+  validates :cannot_save_except_specific_format
 
   def cannot_save_except_specific_format
-    errors.add(:quantity, "数量を入力してください") if quantity.blank?
-
-    specific_format = '\A[0-9０-９]+[ 　]*[\/／0-9０-９]*[ 　]*[0-9０-９]+'
-    # specific_formatと合致していれば変換
-    if /#{specific_format}/.match?(quantity)
-      # 空白を削除, 全角があれば半角に
-      quantity.gsub(" " | "　", "").tr("／", "/").strip!.tr('０-９', '0-9')
-              .else
-      errors.add(:quantity, "例にしたがって入力してください") if quantity.blank?
+    if quantity.blank?
+      errors.add(:quantity, "数量を入力してください")
+    else
+      specific_format = '\A[0-9０-９]+[ 　]*[\/／0-9０-９]*[ 　]*[0-9０-９]+'
+      # specific_formatと合致していれば変換して保存
+      if /#{specific_format}/.match?(quantity)
+        # 空白を削除, 全角があれば半角に
+        quantity.gsub(" " | "　", "").tr("／", "/").strip!.tr('０-９', '0-9')
+      else
+        errors.add(:quantity, "例にしたがって入力してください") if quantity.blank?
+      end
     end
   end
 end
