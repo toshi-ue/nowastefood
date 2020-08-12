0ae2b0065774dac63b9b3bf0816ac5b3ef8571f4
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-08-12 12:33:41 +0900

    update: テストを修正

--- spec/requests/users/stock_spec.rb
+++ spec/requests/users/stock_spec.rb
@@ -8,68 +8,75 @@ RSpec.describe "Users::Stocks", type: :request do
   end
 
   describe "GET /index" do
-    before do
-      (1..2).each do |n|
-        stock = create(:stock, quantity: ((n + 1) * 100).to_s)
-        # puts stock
-        # byebug
-      end
-    end
-
     context "ログインしている場合" do
-      it "リクエストが成功すること" do
+      it "indexページを表示すること" do
+        (1..2).each do |n|
+          create(:stock, user_id: @user.id, quantity: (n * 100).to_s)
+        end
         get users_stocks_path
         expect(response.status).to eq 200
-      end
-
-      it "テンプレートの取得、インスタンスの取得ができる" do
-        # get users_stocks_path
-        # expect(response.body).to include("100")
+        expect(response.body).to include("100")
+        expect(response.body).to include("200")
       end
     end
 
     context "ログインしていない場合" do
-      it "サインインページにリダイレクトする" do
+      it "サインインページへリダイレクトする" do
         sign_out @user
         get users_stocks_path
+        expect(response.status).to eq 302
         expect(response).to redirect_to(new_user_session_path)
+        follow_redirect!
+        expect(response.body).to include("メールアドレス")
       end
     end
   end
 
   describe "GET /new" do
     context "ログインしている場合" do
-      it "リクエストが成功すること" do
+      it "newページを表示する" do
         get new_users_stock_path
         expect(response.status).to eq 200
+        expect(response.body).to include("食材名")
       end
     end
 
     context "ログインしていない場合" do
-      it "サインインページにリダイレクトする" do
+      it "サインインページへリダイレクトする" do
         sign_out @user
         get new_users_stock_path
         expect(response).to redirect_to(new_user_session_path)
+        follow_redirect!
+        expect(response.body).to include("メールアドレス")
       end
     end
   end
 
   describe "POST /create" do
-    context "ログインしている場合" do
-      context "値が妥当な場合" do
-        it "リクエストが成功すること" do
-          expect{
-            post users_stocks_path,
-                 params: { stock: attributes_for(:stock, user_id: @user.id, quantity: "100") }
-          }.to change{Stock.count}.by(1)
-        end
-
-        it "新規レコードが保存されること"
-        it "(妥当なページへ)リダイレクトされること"
+    context "値が妥当な場合" do
+      it "indexアクションへリダイレクトされること" do
+        rawmaterial = create(:rawmaterial)
+        expect{
+          post users_stocks_path,
+                params: { stock: attributes_for(:stock, user_id: @user.id, rawmaterial_id: rawmaterial.id, quantity: "1/2") }
+        }.to change{Stock.count}.by(1)
+        # expect(response.status).to eq 200
+        expect(response).to redirect_to(users_stocks_path)
+        follow_redirect!
+        expect(response.body).to include("家にある食材")
       end
     end
 
-    context "ログインしていない場合" do
+    context "値が不当な場合" do
+      it "newアクションにリダイレクトする" do
+        rawmaterial = create(:rawmaterial)
+        expect{
+            post users_stocks_path,
+                  params: { stock: attributes_for(:stock, user_id: @user.id, rawmaterial_id: rawmaterial.id, quantity: "") }
+        }.to change{Stock.count}.by(0)
+        expect(response.status).to eq 200
+        expect(response.body).to include("食材名")
+      end
     end
   end
 
