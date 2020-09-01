b7f4a39b494649c0464355b92eee844b2c667b8d
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-09-01 20:32:40 +0900

    update: テストを追加

--- app/models/foodstuff.rb
+++ app/models/foodstuff.rb
@@ -1,11 +1,8 @@
 class Foodstuff < ApplicationRecord
-  before_save :convert_specific_format
+  before_validation :convert_specific_format # 空白を削除, 全角があれば半角に
   belongs_to :cuisine
   belongs_to :rawmaterial
-
-  # FIXME: validationはかかる
-  #         ただしvalidationメッセージは"数量を入力してください"と表示される
-  validates :quantity, presence: true, format: { with: %r{\A[1-9１-９]*[/／]*[0-9０-９]*\z}, message: "は分数もしくは数字(整数)で入力してください" }
+  validates :quantity, presence: true, format: { with: %r{\A[1-9１-９]*[/／]*[0-9０-９]*\z}, message: "は分数もしくは数字で入力してください(例: 1/2, 120など)" }
   # FIXME: validationなしでもrawmaterial_id は validationがかかる
   #         なぜ?
   # validates :rawmaterial_id, presence: { message: "を追加してください"}
@@ -13,28 +10,7 @@ class Foodstuff < ApplicationRecord
   include RankedModel
   ranks :row_order, with_same: :cuisine_id
 
-  # quantityのvalidation
-  #  validationがokなら値を半角に変換して保存したい
-  #   もとはstring
-  #   変換する必要あり
-  #   整数(大文字、小文字)ならok
-  #     ok
-  #       1
-  #       １２
-  #       1３
-  #   分数(大文字、小文字)ならok
-  #     ok
-  #       1/1
-  #       1 / 3
-  #       １／１
-  #       1　／ 　　１
-  #   それ以外はng
-  #   okであれば大文字の場合は小文字に変換
-
-  # validates :cannot_save_except_specific_format
-
   def convert_specific_format
-    # 空白を削除, 全角があれば半角に
-    self.quantity = quantity.gsub(/ 　/, "").tr("／", "/").strip.tr('０-９', '0-9')
+    self.quantity = quantity.gsub(/ |　/, "").tr("／", "/").strip.tr('０-９', '0-9')
   end
 end
--- spec/models/foodstuff_spec.rb
+++ spec/models/foodstuff_spec.rb
@@ -1,5 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe Foodstuff, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "quantityカラムのバリデーション" do
+    context "OK" do
+      it "数字のみ(半角)であること" do
+        foodstuff = build(:foodstuff, :only_half_size_number)
+        foodstuff.valid?
+        expect(foodstuff).to be_valid
+      end
+
+      it "数字のみ(全角)であること" do
+        foodstuff = build(:foodstuff, :only_full_width_number)
+        foodstuff.valid?
+        expect(foodstuff).to be_valid
+      end
+
+      it "分数であること" do
+        foodstuff = build(:foodstuff, :only_full_width_number)
+        foodstuff.valid?
+        expect(foodstuff).to be_valid
+      end
+
+      it "スペースを含んでいる数字であること" do
+        foodstuff = build(:foodstuff, :only_half_size_number_with_space)
+        foodstuff.valid?
+        expect(foodstuff).to be_valid
+      end
+
+      it "スペースを含んでいる分数であること" do
+        foodstuff = build(:foodstuff, :full_width_fraction_with_space)
+        foodstuff.valid?
+        expect(foodstuff).to be_valid
+      end
+    end
+
+    context "NG" do
+      it "/、数字、スペース以外の文字列を含むこと" do
+        foodstuff = build(:foodstuff, quantity: "aaa")
+        foodstuff.valid?
+        expect(foodstuff).to be_invalid
+      end
+    end
+  end
+
+  # TODO: 別ブランチでテスト
+  describe "cuisine_idカラムのバリデーション" do
+    # context "NG" do
+    #   it "同一のcuisine_id, rawmaterial_idであれば無効であること"
+    # end
+  end
+
+  describe "rawmaterial_idカラムのバリデーション" do
+    # context "NG" do
+    #   it "同一のcuisine_id, rawmaterial_idであれば無効であること"
+    # end
+  end
+
 end
