4565bda17c9f00ba6753787cd13cf3980d35bab6
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-09-04 08:25:48 +0900

    複数カラムを使用したユニーク制約を追加

--- app/models/foodstuff.rb
+++ app/models/foodstuff.rb
@@ -2,6 +2,7 @@ class Foodstuff < ApplicationRecord
   before_validation :convert_specific_format # 空白を削除, 全角があれば半角に
   belongs_to :cuisine
   belongs_to :rawmaterial
+  validate :uniqueness_rawmaterial_id
   validates :quantity, presence: true, format: { with: %r{\A[1-9１-９]*[/／]*[0-9０-９]*\z}, message: "は分数もしくは数字で入力してください(例: 1/2, 120など)" }
   # FIXME: validationなしでもrawmaterial_id は validationがかかる
   #         なぜ?
@@ -13,4 +14,10 @@ class Foodstuff < ApplicationRecord
   def convert_specific_format
     self.quantity = quantity.gsub(/ |　/, "").tr("／", "/").strip.tr('０-９', '0-9')
   end
+
+  private
+    def uniqueness_rawmaterial_id
+      foodstuff = Foodstuff.find_by(cuisine_id: self.cuisine_id, rawmaterial_id: self.rawmaterial_id)
+      errors.add(:base, "1つの料理に同じ材料を登録できません") if foodstuff && foodstuff.id != self.id
+    end
 end
--- spec/models/foodstuff_spec.rb
+++ spec/models/foodstuff_spec.rb
@@ -1,34 +1,51 @@
 require 'rails_helper'
 
 RSpec.describe Foodstuff, type: :model do
-  describe "quantityカラムのバリデーション" do
+  describe "#convert_specific_format" do
+    let(:foodstuff){ Foodstuff.new(quantity: quantity) }
+    subject { foodstuff.convert_specific_format }
     context "OK" do
-      it "数字のみ(半角)であること" do
-        foodstuff = build(:foodstuff, :only_half_size_number)
-        foodstuff.valid?
-        expect(foodstuff).to be_valid
+      context "半角数字のみの場合" do
+        let(:quantity) { "1" }
+        it { is_expected.to eq "1" }
       end
 
-      it "数字のみ(全角)であること" do
-        foodstuff = build(:foodstuff, :only_full_width_number)
+      context "全角数字のみの場合" do
+        let(:quantity) { "１" }
+        it { is_expected.to eq("1") }
+      end
+
+      context "スペースを含んでいる数字の場合" do
+        let(:quantity) { " 1　" }
+        it { is_expected.to eq "1" }
+      end
+
+      context "分数であること" do
+        let(:quantity) { " 1/2" }
+        it { is_expected.to eq "1/2" }
+      end
+
+      context "スペースを含んでいる分数であること" do
+        let(:quantity) { " 1/2　" }
+        it { is_expected.to eq "1/2" }
+      end
+      context "/、数字、スペース以外の文字列を含むこと" do
+        let(:quantity) { "aaa" }
+        it { is_expected.to "aaa" }
+      end
+    end
+  end
+
+  describe "quantity" do
+    context "OK" do
+      it "数字のみであること" do
+        foodstuff = build(:foodstuff, :only_half_size_number)
         foodstuff.valid?
         expect(foodstuff).to be_valid
       end
 
       it "分数であること" do
         foodstuff = build(:foodstuff, :only_full_width_number)
-        foodstuff.valid?
-        expect(foodstuff).to be_valid
-      end
-
-      it "スペースを含んでいる数字であること" do
-        foodstuff = build(:foodstuff, :only_half_size_number_with_space)
-        foodstuff.valid?
-        expect(foodstuff).to be_valid
-      end
-
-      it "スペースを含んでいる分数であること" do
-        foodstuff = build(:foodstuff, :full_width_fraction_with_space)
         foodstuff.valid?
         expect(foodstuff).to be_valid
       end
@@ -43,17 +60,14 @@ RSpec.describe Foodstuff, type: :model do
     end
   end
 
-  # TODO: 別ブランチでテスト
-  describe "cuisine_idカラムのバリデーション" do
-    # context "NG" do
-    #   it "同一のcuisine_id, rawmaterial_idであれば無効であること"
-    # end
+  describe "cuisine_id" do
+    context "NG" do
+      it "同一のcuisine_id, rawmaterial_idであれば無効であること" do
+        foodstuff = create(:foodstuff)
+        foodstuff_overlapping_cuisine_and_rawmaterial_id = build(:foodstuff, cuisine_id: foodstuff.cuisine_id, rawmaterial_id: foodstuff.rawmaterial_id)
+        foodstuff_overlapping_cuisine_and_rawmaterial_id.valid?
+        expect(foodstuff_overlapping_cuisine_and_rawmaterial_id).to be_invalid
+      end
+    end
   end
-
-  describe "rawmaterial_idカラムのバリデーション" do
-    # context "NG" do
-    #   it "同一のcuisine_id, rawmaterial_idであれば無効であること"
-    # end
-  end
-
 end
