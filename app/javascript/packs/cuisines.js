ac1d8200f1c263b9abb84c5bec821f0bf677ac2f
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-09-25 17:36:19 +0900

    update: ユーザーログイン画面への遷移を追加、ajaxのテストを追加 #94

--- app/javascript/packs/cuisines.js
+++ app/javascript/packs/cuisines.js
@@ -1,28 +1,15 @@
 $(document).ready(function () {
-  console.log($("#tab-foodstuff"))
   $("#tab-foodstuff").css("font-weight", "bold");
 
-  // const tabsArea = document.getElementById('foodstuff-and-procedure')
-  // const tabs = tabsArea.getElementsByClassName('nav-link')
-  // $('#fp-tab a').on('click', function (e) {
-  //   tabs.css("font-weight", "normal");
-  //   console.log(e)
-  //   console.log(this)
-  //   $(this).css("font-weight", "bold");
-  //   // $(this).tab('show')
-  //   // console.log($(this))
-  // })
-
-  // FIXME: タブ切り替え時に
-  //           アクティブなときはfont-weightをboldに
-  //           非アクティブなときはfont-weightをnormalにしたい
-  //        が、下記のイベントが発火してくれない
-  // https://getbootstrap.com/docs/4.5/components/navs/　の一番下のコード
-  // https://stackoverflow.com/questions/30468111/bootstrap-shown-bs-tab-event-not-working
-  $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-    console.log(e)
-    console.log(e.target) // newly activated tab
-    console.log(e.relatedTarget) // previous active tab
+  $('a[data-toggle="tab"]').on('click', function (e) {
+    let clickedTab = $(e.target).attr("id")
+    if (clickedTab === "tab-procedure") {
+      $("#tab-procedure").css("font-weight", "bold");
+      $("#tab-foodstuff").css("font-weight", "normal");
+    } else {
+      $("#tab-foodstuff").css("font-weight", "bold");
+      $("#tab-procedure").css("font-weight", "normal");
+    }
   })
 });
 
