5547482d976edf59b1725d9a6fa73d8ede41cb65
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-10-30 20:04:36 +0900

    add: userモデルのテストを追加

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,6 +4,7 @@ FactoryBot.define do
     sequence(:email) { |n| "heascook_user#{n + 2}@ahk.jp" }
     password { 'password' }
     password_confirmation { 'password' }
+    profile_image { Faker::Avatar.image(slug: "my-own-slug", size: "50x50") }
     confirmation_sent_at { Time.zone.today - 2.days }
     confirmed_at { Time.zone.today - 1.day }
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # before do
+  #   @user = create(:user)
+  #   sign_in @user
+  #   get root_path
+  # end
+
+  describe 'nickname' do
+    context '更新するとき' do
+      it '10文字以内のとき更新できること' do
+        user = build(:user, nickname: "a" * 10)
+        expect(user.save(context: :update_profile)).to be_truthy
+      end
+
+      it '11文字のとき更新できないこと' do
+        user = build(:user, nickname: "a" * 11)
+        expect(user.save(context: :update_profile)).to be_falsy
+      end
+
+      it 'nilのとき更新できないこと' do
+        user = build(:user, nickname: nil)
+        expect(user.save(context: :update_profile)).to be_falsy
+      end
+    end
+  end
 end
