17593f386138cf28d14a1b13b371ff869f40dd8e
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-11-16 17:50:50 +0900

    wip 原因箇所?

--- spec/support/factory_bot.rb
+++ spec/support/factory_bot.rb
@@ -3,6 +3,7 @@ RSpec.configure do |config|
   #   テスト中に、FactoryBot.create(:user) ではなく、create(:user) と書けるようになる。
   config.include FactoryBot::Syntax::Methods
 
+  # ここが原因?
   # springが原因でfactoryが正しく読み込まれないことを防ぐ
   config.before :all do
     FactoryBot.reload
