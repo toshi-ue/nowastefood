a216802ae66c9d10838bc381737fff812ef05040
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-11-23 16:15:02 +0900

    fix: factoryの画像ファイルのパスを変更

--- spec/factories/cuisines.rb
+++ spec/factories/cuisines.rb
@@ -6,6 +6,6 @@ FactoryBot.define do
     # sequence(:calories) {|n| n }
     sequence(:cooking_time) { |n| n }
     sequence(:description) { |n| "料理名#{n}の説明" }
-    sequence(:main_image) { File.new("#{Rails.root}/spec/factories/images/test.jpg") }
+    sequence(:main_image) { File.new("#{Rails.root}/app/assets/images/test.jpg") }
   end
 end
