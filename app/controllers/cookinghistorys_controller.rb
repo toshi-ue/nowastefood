22203862ac02b55c8c208a9cbc96f1ad34316957
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-12-01 18:44:46 +0900

    update: 有料ユーザーと無料ユーザーの検索範囲を指定

--- app/controllers/cookinghistorys_controller.rb
+++ app/controllers/cookinghistorys_controller.rb
@@ -2,6 +2,10 @@ class CookinghistorysController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    current_user.todaysmenus
+    @cookinghistorys = if current_user.subscribed
+                         current_user.todaysmenus
+                       else
+                         current_user.todaysmenus.where(created_at: Time.zone.now.ago(3.days).beginning_of_day..Time.zone.now.end_of_day)
+                       end
   end
 end
