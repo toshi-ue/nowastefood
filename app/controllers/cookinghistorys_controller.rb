b21fc7928153e964f6a6b1093a94c9ae66cef117
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-12-04 18:14:37 +0900

    update: 日数の表示を変更

--- app/controllers/cookinghistorys_controller.rb
+++ app/controllers/cookinghistorys_controller.rb
@@ -2,11 +2,12 @@ class CookinghistorysController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    now = Time.zone.now
     @cuisine_ids_already_in = current_user.todaysmenus.search_in_today.pluck(:cuisine_id)
     @cookinghistorys = if current_user.subscribed
-                         current_user.todaysmenus.includes(:cuisine).order(created_at: "DESC")
+                         current_user.todaysmenus.includes(:cuisine).where("created_at <= ?", now.yesterday).order(created_at: "DESC")
                        else
-                         current_user.todaysmenus.where(created_at: Time.zone.now.ago(3.days).beginning_of_day..Time.zone.now.end_of_day).order(created_at: "DESC")
+                         current_user.todaysmenus.where(created_at: now.ago(4.days).beginning_of_day..now.yesterday).order(created_at: "DESC")
                        end
   end
 
@@ -20,7 +21,8 @@ class CookinghistorysController < ApplicationController
   end
 
   def remove_from_todays_menus
-    @todaysmenu = current_user.todaysmenus.find_by(cuisine_id: params[:cuisine_id], created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    now = Time.zone.now
+    @todaysmenu = current_user.todaysmenus.find_by(cuisine_id: params[:cuisine_id], created_at: now.beginning_of_day..now.end_of_day)
     @todaysmenu.destroy
     redirect_to cookinghistorys_path, flash: { notice: "#{@todaysmenu.cuisine.name} deleted." }
   end
--- app/helpers/cookinghistorys_helper.rb
+++ app/helpers/cookinghistorys_helper.rb
@@ -1,2 +1,9 @@
 module CookinghistorysHelper
+  def time_ago(time)
+    if time.between?(Time.zone.now.yesterday.beginning_of_day, Time.zone.now.yesterday.end_of_day)
+      "昨日"
+    else
+      time_ago_in_words(time)
+    end
+  end
 end
