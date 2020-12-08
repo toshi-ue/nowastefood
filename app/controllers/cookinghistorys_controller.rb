65ab1851163048ceb6a867af19ace91028979af9
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-12-08 15:14:21 +0900

    wip

--- app/controllers/cookinghistorys_controller.rb
+++ app/controllers/cookinghistorys_controller.rb
@@ -4,10 +4,11 @@ class CookinghistorysController < ApplicationController
   def index
     now = Time.zone.now
     @cuisine_ids_already_in = current_user.todaysmenus.search_in_today.pluck(:cuisine_id)
+    # binding.pry
     @cookinghistorys = if current_user.subscribed
                          current_user.todaysmenus.includes(:cuisine).where("created_at < ?", now.to_date).order(created_at: "DESC")
                        else
-                         current_user.todaysmenus.includes(:cuisine).where(created_at: now.ago(4.days).beginning_of_day..now.yesterday.to_date).order(created_at: "DESC")
+                         current_user.todaysmenus.includes(:cuisine).where(created_at: now.ago(4.days).to_date..now.yesterday.to_date).order(created_at: "DESC")
                        end
   end
 
--- app/helpers/cookinghistorys_helper.rb
+++ app/helpers/cookinghistorys_helper.rb
@@ -3,7 +3,7 @@ module CookinghistorysHelper
     if time.between?(Time.zone.now.yesterday.beginning_of_day, Time.zone.now.yesterday.end_of_day)
       "昨日"
     else
-      time_ago_in_words(time)
+      "#{time_ago_in_words(time)}前"
     end
   end
 end
