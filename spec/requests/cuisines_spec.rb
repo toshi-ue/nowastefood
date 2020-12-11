33346f7c42c69ee2f5caeabb90b0f48d553d8d0b
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-12-11 15:36:45 +0900

    update: cuisineのテストを追加(ユーザー側)

--- spec/requests/cuisines_spec.rb
+++ spec/requests/cuisines_spec.rb
@@ -4,12 +4,12 @@ RSpec.describe "Cuisines", type: :request do
   before do
     @user = create(:user)
     sign_in @user
+    @cuisine = create(:cuisine)
   end
 
   describe "GET /show" do
     it "レシピ詳細が表示されること" do
       sign_out @user
-      @cuisine = create(:cuisine)
       @rawmaterial = create(:rawmaterial)
       @foodstuff = create(:foodstuff, cuisine_id: @cuisine.id, rawmaterial_id: @rawmaterial.id)
       get cuisine_path @cuisine
@@ -24,31 +24,46 @@ RSpec.describe "Cuisines", type: :request do
   describe "POST /add_favorite" do
     context "ユーザーでログインしているとき" do
       it "favoriteに追加されること" do
-        @cuisine = create(:cuisine)
         expect do
-          post cuisine_add_favorite_path @cuisine.id, xhr: true
+          post cuisine_add_favorite_path(@cuisine.id), xhr: true
         end.to change(Favorite, :count).by(1)
-        # expect(@user.already_favorite?(@cuisine)).to eq(true)
+        expect(@user.already_favorite?(@cuisine)).to eq(true)
       end
     end
   end
 
-  # describe "POST /add_menu" do
-  #   context "ユーザーでログインしているとき" do
-  #     it "todaysmenuに追加されること" do
-  #     end
-  #   end
-  # end
+  describe "POST /add_menu" do
+    context "ユーザーでログインしているとき" do
+      it "todaysmenuに追加されること" do
+        expect do
+          post cuisine_add_menu_path(@cuisine.id), xhr: true
+        end.to change(Todaysmenu, :count).by(1)
+        expect(@user.already_menu_in_today?(@cuisine.id)).to eq(true)
+      end
+    end
+  end
 
-  # describe "/remove_favorite" do
-  #   context "ユーザーでログインしているとき" do
-  #     it "favoriteから削除されること"
-  #   end
-  # end
+  describe "DELTE /remove_favorite" do
+    context "ユーザーでログインしているとき" do
+      it "favoriteから削除されること" do
+        favorite = create(:favorite, user_id: @user.id, cuisine_id: @cuisine.id)
+        expect do
+          delete cuisine_remove_favorite_path(@cuisine.id), xhr: true
+        end.to change(Favorite, :count).by(-1)
+        expect(@user.already_favorite?(@cuisine)).to eq(false)
+      end
+    end
+  end
 
-  # describe "remove_menu" do
-  #   context "ユーザーでログインしているとき" do
-  #     it "todaysmenuから削除されること"
-  #   end
-  # end
+  describe "DELETE /remove_menu" do
+    context "ユーザーでログインしているとき" do
+      it "todaysmenuから削除されること" do
+        todaysmenu = create(:todaysmenu, user_id: @user.id, cuisine_id: @cuisine.id)
+        expect do
+          delete cuisine_remove_menu_path(@cuisine.id), xhr: true
+        end.to change(Todaysmenu, :count).by(-1)
+        expect(@user.already_menu_in_today?(@cuisine.id)).to eq(false)
+      end
+    end
+  end
 end
