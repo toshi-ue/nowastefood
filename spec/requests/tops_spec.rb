21efa911505358f34a8f769c7c371cb2dc9f7aaa
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2020-12-14 17:36:23 +0900

    update: トップページのテストを修正

--- spec/requests/tops_spec.rb
+++ spec/requests/tops_spec.rb
@@ -1,20 +1,11 @@
-# TODO: specのテストを追加する
 require 'rails_helper'
 
 RSpec.describe "Tops", type: :request do
-
   describe "GET /about" do
-    it "returns http success" do
-      get "/tops/about"
-      expect(response).to have_http_status(:success)
+    it "aboutページが表示されること" do
+      get tops_about_path
+      expect(response.status).to eq 200
+      expect(response.body).to include "make Cooking Menu Easier"
     end
   end
-
-  describe "GET /mygithub" do
-    it "returns http success" do
-      get "/tops/mygithub"
-      expect(response).to have_http_status(:success)
-    end
-  end
-
 end
