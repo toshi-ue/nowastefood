6ea0c8c2ae84d42f7c6ea65ca0a8bd5e21f7167d
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2021-03-24 17:54:52 +0900

    update: アプリ内の共通変数を変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
   add_flash_types :success, :info, :warning, :danger
+  before_action :set_app_name
 
-  @appname = "heasecook"
+  def set_app_name
+    @appname = "Coome"
+  end
 end
