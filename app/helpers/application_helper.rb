2f2062773a67178d43c10a95601a436e07eefd41
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2021-04-01 17:18:58 +0900

    update: 管理者画面のサイドバーを修正

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,12 @@ module ApplicationHelper
       'text-black-50'
     end
   end
+
+  def add_class_nav_link(current_page, should_adaptive_action_name)
+    if current_page == should_adaptive_action_name
+      'nav-link active'
+    else
+      'nav-link'
+    end
+  end
 end
