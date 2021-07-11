f1163c227298fe9de6ada28f8305927f347976ca
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2021-07-11 15:23:13 +0900

    update: profile_imageカラムをtraitに切り分け

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,8 +4,11 @@ FactoryBot.define do
     sequence(:email) { |n| "user#{n + 2}@example.com" }
     password { 'password' }
     password_confirmation { 'password' }
-    profile_image { Rack::Test::UploadedFile.new("#{Rails.root}/app/assets/images/test.jpg", "image/jpeg") }
     confirmation_sent_at { Time.zone.today - 2.days }
     confirmed_at { Time.zone.today - 1.day }
+
+    trait :with_avatar do
+      profile_image { Rack::Test::UploadedFile.new("#{Rails.root}/app/assets/images/test.jpg", "image/jpeg") }
+    end
   end
 end
