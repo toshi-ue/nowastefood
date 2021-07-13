1d3ecc785091f1794df05bec156eaafbfc82f623
toshi-ue <toshihiro.u.1987.02.07@gmail.com>
2021-07-13 16:55:08 +0900

    update: 管理者トップ画面を変更

--- app/controllers/managers/tops_controller.rb
+++ app/controllers/managers/tops_controller.rb
@@ -3,5 +3,11 @@ class Managers::TopsController < ApplicationController
   layout 'manager'
 
   def dashboard
+    @cuisines = Cuisine.all
+    @favorites = Favorite.all
+    @users = User.all
+    @new_users = @users.where(created_at: Time.zone.now.all_day)
+    @mau = @users.where(last_sign_in_at: Time.zone.now.all_month)
+    @rawmaterials = Rawmaterial.all
   end
 end
