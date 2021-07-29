078c3ff851bc01d0b71f0f5f7c7ee9353291f50c
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-07-29 16:45:07 +0900

    update: stocksの一覧画面のデザインを修正

--- app/helpers/stocks_helper.rb
+++ app/helpers/stocks_helper.rb
@@ -2,14 +2,18 @@ module StocksHelper
   def change_back_color(stock, rawmaterials_and_quantity_will_be_consumed, left_days)
     remaining_quantity = rawmaterials_and_quantity_will_be_consumed.key?(stock.rawmaterial_id) ? @rawmaterials_and_quantity_will_be_consumed[stock.rawmaterial_id] : -1
 
+    # binding.pry
     if remaining_quantity > 0
       if Rational(stock.quantity) - remaining_quantity <= 0
         @rawmaterials_and_quantity_will_be_consumed[stock.rawmaterial_id] = -1
+        @css_name = 'will-consume'
         'will-consume'
       end
     elsif left_days == 1
+      @css_name = 'last-day'
       'last-day'
     elsif left_days < 1
+      @css_name = 'oops'
       'oops'
     end
   end
