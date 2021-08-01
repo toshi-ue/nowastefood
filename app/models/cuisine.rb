fd7f4c31b16965c43d0d1b8153eb582582ff6784
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-08-01 15:13:53 +0900

    update: dependentオプションを追加

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -36,9 +36,3 @@ class Cuisine < ApplicationRecord
     "汁物"
   ].freeze
 end
-
-# [has_many through のアソシエーションに条件を追加する - Qiita](https://qiita.com/QUANON/items/a58ff3960b43af472bfb)
-
-# [関連するモデルの条件で検索したい - Qiita](https://qiita.com/ishidamakot/items/7dba557d764362a828ff#%E7%89%B9%E5%AE%9A%E3%81%AE%E5%95%86%E5%93%81%E3%82%92%E6%B3%A8%E6%96%87%E3%81%97%E3%81%9F%E9%A1%A7%E5%AE%A2%E3%82%92%E6%8A%BD%E5%87%BA)
-
-# [ActiveRecordのjoinsとpreloadとincludesとeager_loadの違い - Qiita](https://qiita.com/k0kubun/items/80c5a5494f53bb88dc58)
--- app/models/rawmaterial.rb
+++ app/models/rawmaterial.rb
@@ -2,6 +2,6 @@ class Rawmaterial < ApplicationRecord
   validates :name, presence: true, uniqueness: true
   belongs_to :foodcategory
   belongs_to :unit
-  has_many :foodstuffs
+  has_many :foodstuffs, dependent: :destroy
   has_many :cuisines, through: :foodstuffs
 end
