436145421ba284479fc4a32220414d2a03af2a6f
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-08-07 10:49:32 +0900

    update: デザインをレスポンシブに修正

--- app/controllers/manageownfoodstuffs_controller.rb
+++ app/controllers/manageownfoodstuffs_controller.rb
@@ -3,6 +3,7 @@ class ManageownfoodstuffsController < ApplicationController
   def new
     @cuisine = Cuisine.find(params[:cuisine_id])
     @foodstuffs = Foodstuff.includes(rawmaterial: :unit).where(cuisine_id: params[:cuisine_id])
+    @procedures = Procedure.where(cuisine_id: params[:cuisine_id])
     @rawmaterials = Rawmaterial.limit(5)
     @foodstuff = Foodstuff.new
   end
@@ -13,8 +14,9 @@ class ManageownfoodstuffsController < ApplicationController
       redirect_to new_manageownfoodstuff_path(cuisine_id: @foodstuff.cuisine_id), flash: { notice: "#{@foodstuff.rawmaterial.name} が追加されました" }
     else
       @cuisine = Cuisine.find(@foodstuff.cuisine_id)
-      @rawmaterials = Rawmaterial.limit(5)
       @foodstuffs = Foodstuff.includes(rawmaterial: :unit).where(cuisine_id: params[:cuisine_id])
+      @procedures = Procedure.where(cuisine_id: params[:cuisine_id])
+      @rawmaterials = Rawmaterial.limit(5)
       params[:cuisine_id] = @foodstuff.cuisine_id
       render 'new'
     end
