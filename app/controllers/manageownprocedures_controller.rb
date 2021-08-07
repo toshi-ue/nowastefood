2afaea84bf8550092d0285dec9e8cd2d3ad2a6ca
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-08-07 16:41:55 +0900

    add: 更新、削除フローを追加

--- app/controllers/manageownprocedures_controller.rb
+++ app/controllers/manageownprocedures_controller.rb
@@ -20,6 +20,33 @@ class ManageownproceduresController < ApplicationController
     end
   end
 
+  def edit
+    @procedure = Procedure.find(params[:id])
+    @cuisine = Cuisine.find(@procedure.cuisine_id)
+    @procedures = Procedure.where(cuisine_id: @cuisine.id)
+    @foodstuffs = Foodstuff.includes(rawmaterial: :unit).where(cuisine_id: @cuisine.id).where.not(params[:id])
+  end
+
+  def update
+    if @foodstuff.update(foodstuff_params)
+      redirect_to managecuisine_path(@foodstuff.cuisine_id), flash: { notice: "変更されました" }
+    else
+      @cuisine = Cuisine.find(@foodstuff.cuisine_id)
+      @foodstuffs = Foodstuff.includes(rawmaterial: :unit).where(cuisine_id: params[:cuisine_id])
+      @procedures = Procedure.where(cuisine_id: @cuisine.id)
+      @rawmaterials = Rawmaterial.limit(5)
+      params[:cuisine_id] = @foodstuff.cuisine_id
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @procedure = Procedure.find(params[:id])
+    @procedure.destroy
+    redirect_to new_manageownprocedure_path(cuisine_id: @procedure.cuisine_id), flash: { notice: "手順が1つ削除されました" }
+  end
+
+  private
 
   def set_procedure
     @procedure = Procedure.find(params[:id])
