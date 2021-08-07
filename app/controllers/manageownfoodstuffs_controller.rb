3b768c59795f5acdad1b8fddff88c21202508fbc
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-08-07 11:22:45 +0900

    update: 変更画面を追加

--- app/controllers/manageownfoodstuffs_controller.rb
+++ app/controllers/manageownfoodstuffs_controller.rb
@@ -1,5 +1,7 @@
 class ManageownfoodstuffsController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_foodstuff, only: [:edit, :update]
+
   def new
     @cuisine = Cuisine.find(params[:cuisine_id])
     @foodstuffs = Foodstuff.includes(rawmaterial: :unit).where(cuisine_id: params[:cuisine_id])
@@ -22,6 +24,25 @@ class ManageownfoodstuffsController < ApplicationController
     end
   end
 
+  def edit
+    @cuisine = Cuisine.find(@foodstuff.cuisine_id)
+    @procedures = Procedure.where(cuisine_id: @cuisine.id)
+    @rawmaterials = Rawmaterial.limit(5)
+    @foodstuffs = Foodstuff.includes(rawmaterial: :unit).where(cuisine_id: @cuisine.id).where.not(params[:id])
+  end
+
+  def update
+    if @foodstuff.update(foodstuff_params)
+      redirect_to managecuisine_path(@foodstuff.cuisine_id), flash: { notice: "変更されました" }
+    else
+      @cuisine = Cuisine.find(@foodstuff.cuisine_id)
+      @foodstuffs = Foodstuff.includes(rawmaterial: :unit).where(cuisine_id: params[:cuisine_id])
+      @procedures = Procedure.where(cuisine_id: @cuisine.id)
+      @rawmaterials = Rawmaterial.limit(5)
+      params[:cuisine_id] = @foodstuff.cuisine_id
+      render 'edit'
+    end
+  end
 
   def destroy
     @foodstuff = Foodstuff.find(params[:id])
@@ -30,6 +51,10 @@ class ManageownfoodstuffsController < ApplicationController
   end
 
   private
+
+  def set_foodstuff
+    @foodstuff = Foodstuff.find(params[:id])
+  end
 
   def foodstuff_params
     params.require(:foodstuff).permit(:id, :quantity, :cuisine_id, :rawmaterial_id, :row_order_position)
