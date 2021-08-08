2855e99e728fc0ca890752c1d8c6abc18e517414
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-08-08 18:45:33 +0900

    fix: swiperが動作しない問題を修正

--- app/controllers/tops_controller.rb
+++ app/controllers/tops_controller.rb
@@ -1,10 +1,11 @@
 class TopsController < ApplicationController
   def index
     @japanese_style_cuisines = Cuisine.where(genre_id: 1).limit(5)
-    @western_style_cuisines = Cuisine.where(genre_id: 2).limit(4)
-    @chinese_style_cuisines = Cuisine.where(genre_id: 3).limit(4)
+    @western_style_cuisines = Cuisine.where(genre_id: 2).limit(5)
+    @chinese_style_cuisines = Cuisine.where(genre_id: 3).limit(5)
     @genres = Genre.all
     @tags = ActsAsTaggableOn::Tag.all
+    # binding.pry
   end
 
   def about; end
