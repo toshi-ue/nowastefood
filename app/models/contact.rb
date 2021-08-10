0d7bf3a005c4b728aabd658c1ad139627f08505f
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-08-11 07:53:20 +0900

    update: contactモデルにバリデーションを追加

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,5 @@
 class Contact < ApplicationRecord
+  enum category: { error: 0, request: 1 }, _suffix: true
+  validates :message, presence: true, length: { maximum: 200 }
+  validates :subject, presence: true, length: { maximum: 50 }
 end
