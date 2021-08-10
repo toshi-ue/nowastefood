28be7442e59dce4b63d7e32273ea078ad408ce0b
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-08-11 07:55:13 +0900

    update: 問い合わせのnew, createのアクションを作成

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,22 @@
 class ContactsController < ApplicationController
+  before_action :authenticate_user!
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, flash: { notice: "お問い合わせ内容が送信されました" }
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def contact_params
+    params.require(:contact).permit(:id, :category, :subject, :message).merge(user_name: current_user.nickname)
+  end
 end
