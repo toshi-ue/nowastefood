1b8b738afb3a98e03452d23a4f1f5da7f7017339
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-08-11 16:59:45 +0900

    update: メール送信のテストを追加、不要なファイルを削除

--- spec/factories/contacts.rb
+++ spec/factories/contacts.rb
@@ -1,6 +1,8 @@
 FactoryBot.define do
-  # factory :contact do
-  #   category { "MyString" }
-  #   message { "MyString" }
-  # end
+  factory :contact do
+    category { Contact.categories.keys.sample }
+    user_name { Faker::Name.first_name }
+    sequence(:subject) { |n| "用件 #{n + 1}" }
+    sequence(:message) { |n| "問い合わせ本文 #{n + 1}" }
+  end
 end
