6cb073a9430b38b5203b400ac25a50f277974f23
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-08-17 16:15:25 +0900

    update: rawmaterialとuserのrelationを追加

--- app/models/rawmaterial.rb
+++ app/models/rawmaterial.rb
@@ -1,7 +1,9 @@
 class Rawmaterial < ApplicationRecord
   validates :name, presence: true, uniqueness: true
+  validates :hiragana, presence: true, uniqueness: true
   belongs_to :foodcategory
   belongs_to :unit
+  belongs_to :user
   has_many :foodstuffs, dependent: :destroy
   has_many :cuisines, through: :foodstuffs
 end
