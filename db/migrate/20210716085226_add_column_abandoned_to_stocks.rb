cc7f63620ebf22544bd70c90d1d5841cf224ada8
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-08-22 08:27:23 +0900

    fix: マイグレーションファイルを修正

--- db/migrate/20210716085226_add_column_abandoned_to_stocks.rb
+++ db/migrate/20210716085226_add_column_abandoned_to_stocks.rb
@@ -1,9 +1,9 @@
 class AddColumnAbandonedToStocks < ActiveRecord::Migration[6.0]
-  def change
+  def up
     add_column :stocks, :abandoned, :boolean, null: false, defalut: 0, after: :rotted_at
   end
 
   def down
-    remove_column :stocks, :abandoned, :boolean, null: true, defalut: 0,
+    remove_column :stocks, :abandoned, :boolean, null: true, defalut: 0
   end
 end
