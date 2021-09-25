0746287f98f8704b26ee871c9fd1cd0b92e7beaf
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-09-25 19:08:33 +0900

    update: entrypoint.shを修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,11 +4,11 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /webapp/tmp/pids/server.pid
 
-if [! -e "/webapp/tmp"]; then
+if [ ! -e "/webapp/tmp"]; then
   mkdir /webapp/tmp
 fi
 
-if [! -e "/webapp/tmp/sockets"]; then
+if [ ! -e "/webapp/tmp/sockets"]; then
   mkdir /webapp/tmp/sockets
 fi
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
