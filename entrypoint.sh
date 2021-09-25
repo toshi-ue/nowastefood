6c5964d1cac7cf78471be0ebc15791172a112396
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-09-25 18:41:31 +0900

    update: entrypoint.shを修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # Remove a potentially pre-existing server.pid for Rails.
-rm -f /myapp/tmp/pids/server.pid
+rm -f /webapp/tmp/pids/server.pid
 
 if [! -e "/webapp/tmp"]; then
   mkdir /webapp/tmp
