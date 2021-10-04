1aaed38548fe821b04a9dba8b7ee616c13456c41
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-10-04 20:19:36 +0900

    fix: s3の識別子を秘匿化

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -6,12 +6,12 @@ CarrierWave.configure do |config|
   if Rails.env.production?
     config.storage :fog
     config.fog_provider = 'fog/aws'
-    config.fog_directory  = 'my-rails-image'
+    config.fog_directory  = ENV['AWS_S3_PUCKET_NAME']
     config.fog_credentials = {
       provider: 'AWS',
       aws_access_key_id: ENV['AWS_ACCESS_KEY_ID'],
       aws_secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
-      region: 'ap-northeast-1',
+      region: ENV['AWS_REGION'],
       path_style: true
     }
   else
