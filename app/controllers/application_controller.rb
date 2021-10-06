6be5932690598444beda5736fdf13af803226693
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-10-06 14:38:57 +0900

    fix: 本番環境でエラー発生時に表示されるページのlayout指定を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,13 +9,13 @@ class ApplicationController < ActionController::Base
     rescue_from ActiveRecord::RecordNotFound, with: :render_404
 
     def render_404
-      render file: Rails.root.join('public/404.html'), status: :not_found, layout: false
+      render file: Rails.root.join('public/404.html'), status: :not_found
     end
 
     def render_500(e)
       logger.error(e.message)
       logger.error(e.backtrace.join("\n"))
-      render file: Rails.root.join('public/500.html'), status: :internal_server_error, layout: false
+      render file: Rails.root.join('public/500.html'), status: :internal_server_error
     end
   end
 
