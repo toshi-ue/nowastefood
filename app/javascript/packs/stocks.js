3c4980581f1bee854fdd70319ecda5ee4e796702
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-10-08 17:37:20 +0900

    update: stock.jsを修正

--- app/javascript/packs/stocks.js
+++ app/javascript/packs/stocks.js
@@ -5,24 +5,24 @@ import '@ttskch/select2-bootstrap4-theme/dist/select2-bootstrap4.min.css'
 $(function () {
   let action_name = $('body').data('action');
   const csrfToken = document.querySelector('[name="csrf-token"]').getAttribute('content');
-  if ($('#stock_rawmaterial_id option:selected').val()) {
-    $.ajax({
-      type: 'GET',
-      url: '/stocks/search_unit_and_expiry_period',
-      data: { id: $('#stock_rawmaterial_id option:selected').val() },
-      dataType: 'json'
-    }).done(function (data) {
-      $(".input-group-text").text(data.unit_name);
-      $("#expiry_period").text(data.expiry_period + "日");
-    }).fail(function () {
-      console.log("could not get unit name.");
-    })
-  }
+  // if ($('.stock-rawmaterial-search option:selected').val()) {
+  //   $.ajax({
+  //     type: 'GET',
+  //     url: '/stocks/search_unit_and_expiry_period',
+  //     data: { id: $('#stock_rawmaterial_id option:selected').val() },
+  //     dataType: 'json'
+  //   }).done(function (data) {
+  //     $(".input-group-text").text(data.unit_name);
+  //     $("#expiry_period").text(data.expiry_period + "日");
+  //   }).fail(function () {
+  //     console.log("could not get unit name.");
+  //   })
+  // }
 
   // switch (action_name) {
   //   case "new":
   //   case "create":
-      $('#stock_rawmaterial_id').select2({
+  $('.stock-rawmaterial-search').select2({
         ajax: {
           url: '/stocks/search_rawmaterial',
           datatype: 'json',
@@ -38,12 +38,13 @@ $(function () {
             };
           }
         },
+        allowClear: true,
         placeholder: "入力してください",
         theme: 'bootstrap4',
         width: 'auto',
       });
 
-      $('#stock_rawmaterial_id').on('select2:select', function (e) {
+  $('.stock-rawmaterial-search').on('select2:select', function (e) {
         let rawmaterial_id = e.params.data.id;
         $(".input-group-text").text("");
         $("#expiry_period").text("");
