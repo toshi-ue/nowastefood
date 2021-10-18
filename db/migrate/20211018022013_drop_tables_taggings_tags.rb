2bcb1faf21a13aa33420b81baed2a79fa85f7eff
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-10-18 11:23:32 +0900

    update: taggingsテーブルを削除

---
+++ db/migrate/20211018022013_drop_tables_taggings_tags.rb
@@ -0,0 +1,26 @@
+class DropTablesTaggingsTags < ActiveRecord::Migration[6.0]
+  def up
+    drop_table :taggings
+  end
+
+  def down
+    create_table :taggings do |t|
+      t.integer "tag_id"
+      t.string "taggable_type"
+      t.integer "taggable_id"
+      t.string "tagger_type"
+      t.integer "tagger_id"
+      t.string "context", limit: 128
+      t.datetime "created_at"
+      t.index ["context"], name: "index_taggings_on_context"
+      t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
+      t.index ["tag_id"], name: "index_taggings_on_tag_id"
+      t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
+      t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
+      t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
+      t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
+      t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
+      t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_10_13_084423) do
+ActiveRecord::Schema.define(version: 2021_10_18_022013) do
 
   create_table "contacts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "category", default: 0, null: false
@@ -126,25 +126,6 @@ ActiveRecord::Schema.define(version: 2021_10_13_084423) do
     t.datetime "updated_at", null: false
     t.index ["rawmaterial_id"], name: "index_stocks_on_rawmaterial_id"
     t.index ["user_id"], name: "index_stocks_on_user_id"
-  end
-
-  create_table "taggings", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.integer "tag_id"
-    t.string "taggable_type"
-    t.integer "taggable_id"
-    t.string "tagger_type"
-    t.integer "tagger_id"
-    t.string "context", limit: 128
-    t.datetime "created_at"
-    t.index ["context"], name: "index_taggings_on_context"
-    t.index ["tag_id", "taggable_id", "taggable_type", "context", "tagger_id", "tagger_type"], name: "taggings_idx", unique: true
-    t.index ["tag_id"], name: "index_taggings_on_tag_id"
-    t.index ["taggable_id", "taggable_type", "context"], name: "taggings_taggable_context_idx"
-    t.index ["taggable_id", "taggable_type", "tagger_id", "context"], name: "taggings_idy"
-    t.index ["taggable_id"], name: "index_taggings_on_taggable_id"
-    t.index ["taggable_type"], name: "index_taggings_on_taggable_type"
-    t.index ["tagger_id", "tagger_type"], name: "index_taggings_on_tagger_id_and_tagger_type"
-    t.index ["tagger_id"], name: "index_taggings_on_tagger_id"
   end
 
   create_table "tags", id: :integer, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
