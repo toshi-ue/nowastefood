2f2be89b283a58a808567d55fb5f48baacc60469
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-10-19 18:35:04 +0900

    update: cuisineモデルにstatusのenumとvalidationを追加

--- app/models/cuisine.rb
+++ app/models/cuisine.rb
@@ -1,6 +1,13 @@
 class Cuisine < ApplicationRecord
   enum cooking_time: { lt_minutes5: 5, lt_minutes10: 10, lt_minutes20: 20, gt_minutes21: 21 }
   enum genre: { japanese: 1, western: 2, chinese: 3, other: 99 }
+  enum status: { draft: 0, published: 1 }
+
+  validates :description, presence: true
+  validates :genre, inclusion: { in: Cuisine.genres.keys }
+  validates :main_image, presence: { message: "を追加してください" }
+  validates :name, presence: true, uniqueness: true
+  validates :status, inclusion: { in: Cuisine.statuses.keys }
 
   belongs_to :user
   has_many :favorites, dependent: :destroy
@@ -8,10 +15,6 @@ class Cuisine < ApplicationRecord
   has_many :procedures, dependent: :destroy
   has_many :rawmaterials, through: :foodstuffs
   has_many :todaysmenus, dependent: :destroy
-  validates :description, presence: true
-  validates :genre, presence: true
-  validates :name, presence: true, uniqueness: true
-  validates :main_image, presence: { message: "を追加してください" }
 
   scope :eight_contents, -> { includes(:user).limit(8) }
 
