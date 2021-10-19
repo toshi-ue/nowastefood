aabeac7a5b7638ba012919de31979d636078d169
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-10-20 01:56:17 +0900

    update: 必要なseedデータを追加

--- db/fixtures/04_unit.rb
+++ db/fixtures/04_unit.rb
@@ -1,6 +1,6 @@
 # DO NOT MODIFY THIS FILE, it was auto-generated.
 #
-# Date: 2020-11-23 06:03:30 +0000
+# Date: 2021-10-19 16:46:18 +0000
 # Seeding Unit
 # Written with the command:
 #
--- db/fixtures/22_foodstuff.rb
+++ db/fixtures/22_foodstuff.rb
@@ -1,6 +1,6 @@
 # DO NOT MODIFY THIS FILE, it was auto-generated.
 #
-# Date: 2020-11-23 06:03:36 +0000
+# Date: 2021-10-19 16:46:23 +0000
 # Seeding Foodstuff
 # Written with the command:
 #
