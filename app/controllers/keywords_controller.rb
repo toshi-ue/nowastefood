c5f1dc6659487897e1640ff11496d8ebf6443f23
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-10-19 12:59:19 +0900

    update: difficultyカラム削除に伴うmvcの修正

--- app/controllers/keywords_controller.rb
+++ app/controllers/keywords_controller.rb
@@ -5,8 +5,6 @@ class KeywordsController < ApplicationController
     case @enum_key
     when "japanese", "western", "chinese", "other"
       @cuisines = Cuisine.where(genre: @enum_key).includes(:user).page(params[:page]).per(KAMINARI_PAGINATION_COUNT)
-    when "easy", "normal", "hard"
-      @cuisines = Cuisine.where(difficulty: @enum_key).includes(:user).page(params[:page]).per(KAMINARI_PAGINATION_COUNT)
     when "lt_minutes5", "lt_minutes10", "lt_minutes20", "gt_minutes"
       @cuisines = Cuisine.where(cooking_time: @enum_key).includes(:user).page(params[:page]).per(KAMINARI_PAGINATION_COUNT)
     when "not-enum-ranking"
