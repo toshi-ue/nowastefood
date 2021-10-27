4432cbeb584ac67b8cbbb640c2f9d0d56de89bae
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-10-27 10:56:21 +0900

    update: 問い合わせ種別を追加

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 class Contact < ApplicationRecord
-  enum category: { error: 0, request: 1 }, _suffix: true
+  enum category: { error: 0, request: 1, other: 2 }, _suffix: true
   validates :message, presence: true, length: { maximum: 200 }
   validates :subject, presence: true, length: { maximum: 50 }
 end
