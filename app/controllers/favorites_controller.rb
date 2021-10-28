8ef9dff253f3aab6f1812dd866967b04fecccc6c
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-10-28 18:38:47 +0900

    update: 各アクションの記述を修正

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -10,21 +10,21 @@ class FavoritesController < ApplicationController
 
   def create
     @favorite = current_user.favorites.build(cuisine_id: params[:cuisine_id])
-    @favorite.save
+    @favorite&.save!
     @cuisine = Cuisine.find_by(id: params[:cuisine_id])
   end
 
   def destroy
     @favorite = Favorite.find_by(id: params[:id])
-    @favorite&.destroy
-    redirect_to favorites_path, flash: { notice: "Favoriteから削除されました" }
+    @favorite&.destroy!
+    redirect_to favorites_path, flash: { notice: "#{@favorite.cuisine.name} がお気に入りから削除されました" }
   end
 
   def add_todaysmenu_from_favorite
     @todaysmenu = current_user.todaysmenus.build(cuisine_id: params[:cuisine_id], serving_count: current_user.default_serving_count)
     @todaysmenu.save!
     redirect_to favorites_path, flash: { notice: "#{@todaysmenu.cuisine.name}が今日の献立に追加されました" }
-    end
+  end
 
   def remove_todaysmenu_from_favorite
     @todaysmenu = current_user.todaysmenus.not_cooked.find_by(cuisine_id: params[:cuisine_id])
