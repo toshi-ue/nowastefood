6016a393a3dada4219adac0d3621f5fb28b23abf
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-10-28 10:51:47 +0900

    update: ツールチップを表示、ダブルクォーテーションからシングルクォーテーションに変更

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,3 +7,11 @@ import '@fortawesome/fontawesome-free/js/all'
 Rails.start()
 window.$ = window.jQuery = require('jquery');
 require('data-confirm-modal')
+
+// tooltips
+$(function () {
+  $('.histories-tooltip').tooltip();
+  $('.stocks-tooltip').tooltip();
+  $('.todaysmenus-tooltip').tooltip();
+  $('.favorites-tooltip').tooltip();
+});
