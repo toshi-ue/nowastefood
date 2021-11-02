a9a5e72998229d4cfc3bafafb2de1012a610e7ac
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-11-02 17:28:51 +0900

    update: メソッドの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -17,11 +19,11 @@ class User < ApplicationRecord
   mount_uploader :profile_image, ProfileImageUploader
 
   def already_favorite?(cuisine)
-    self.favorites.exists?(cuisine_id: cuisine.id)
+    favorites.exists?(cuisine_id: cuisine.id)
   end
 
   def already_menu_in_today?(cuisine_id)
-    self.todaysmenus.exists?(cuisine_id: cuisine_id, created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
+    todaysmenus.exists?(cuisine_id: cuisine_id, created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day)
   end
 
   def self.create_account_as_guest
