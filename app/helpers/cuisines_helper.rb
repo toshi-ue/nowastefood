65c26d9cb65a883290a6f234dbd27f7dc396df0d
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2021-11-02 17:28:31 +0900

    update: レシピ詳細ページを分割、デザインの修正

--- app/helpers/cuisines_helper.rb
+++ app/helpers/cuisines_helper.rb
@@ -1,4 +1,14 @@
+# frozen_string_literal: true
+
 module CuisinesHelper
+  def owner_name(cuisine)
+    if cuisine.user.present?
+      "by #{cuisine.user.nickname}さん"
+    else
+      "退会user"
+    end
+  end
+
   def disp_foodstuff(instance)
     if instance.ingredient_id.present? && instance.cookedstate_id.present?
       "#{instance.rawmaterial.name}#{instance.ingredient.name}#{instance.cookedstate.name}"
