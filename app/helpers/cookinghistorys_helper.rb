479e8074cb3099f4f0a428219524182da5dc3010
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-01-31 11:46:28 +0900

    ログ表示の削除

--- app/helpers/cookinghistorys_helper.rb
+++ app/helpers/cookinghistorys_helper.rb
@@ -1,13 +1,10 @@
 module CookinghistorysHelper
   def time_ago(time)
     if time.today?
-      puts "a passed"
       "今日"
     elsif time.yesterday?
-      puts "b passed"
       "昨日"
     else
-      puts "c passed"
       "#{time_ago_in_words(time.to_date)}前"
     end
   end
