11a31755483d7dc4253be20fc33f0af3fcb2da67
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-02-02 12:58:19 +0900

    update: entrypoint.shを修正

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,21 +4,21 @@ set -e
 # Remove a potentially pre-existing server.pid for Rails.
 rm -f /webapp/tmp/pids/server.pid
 
-if [ ! -e "/webapp/tmp" ]; then
-  sudo mkdir /webapp/tmp
-fi
+# if [ ! -e "/webapp/tmp" ]; then
+#   mkdir /webapp/tmp
+# fi
 
-if [ ! -e "/webapp/tmp/pids" ]; then
-  sudo mkdir /webapp/tmp/pids
-fi
+# if [ ! -e "/webapp/tmp/pids" ]; then
+#   mkdir /webapp/tmp/pids
+# fi
 
-if [ ! -e "/webapp/tmp/sockets" ]; then
-  sudo mkdir /webapp/tmp/sockets
-fi
+# if [ ! -e "/webapp/tmp/sockets" ]; then
+#   mkdir /webapp/tmp/sockets
+# fi
 
-if [ ! -e "${RAILS_ENV}" = "production" ]; then
-  bundle exec rails webpacker:compile
-  bundle exec rails assets:precompile
-fi
+# if [ ! -e "${RAILS_ENV}" = "production" ]; then
+#   bundle exec rails webpacker:compile
+#   bundle exec rails assets:precompile
+# fi
 # Then exec the container's main process (what's set as CMD in the Dockerfile).
 exec "$@"
