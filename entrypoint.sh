4bca5c99bd6f3ad4f231eabc700157881dd6458f
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-02-04 05:39:09 +0900

    update: update entrypoint.sh [ci skip]

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,6 +8,8 @@ if [ -f "/webapp/tmp/pids/server.pid" ]; then
 fi
 
 if [ ! -e "${RAILS_ENV}" = "production" ]; then
+  # https://railsguides.jp/asset_pipeline.html#%E3%82%A2%E3%82%BB%E3%83%83%E3%83%88%E3%82%92%E3%83%97%E3%83%AA%E3%82%B3%E3%83%B3%E3%83%91%E3%82%A4%E3%83%AB%E3%81%99%E3%82%8B
+  # bundle exec rails assets:precompile RAILS_ENV=production
   bundle exec rails webpacker:install
   bundle exec rails webpacker:compile
   # https://railsguides.jp/asset_pipeline.html#%E3%82%A2%E3%82%BB%E3%83%83%E3%83%88%E3%82%92%E3%83%97%E3%83%AA%E3%82%B3%E3%83%B3%E3%83%91%E3%82%A4%E3%83%AB%E3%81%99%E3%82%8B
