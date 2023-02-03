d21f3a04720a52f25c8d11fa4af0027d8c1250c3
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-02-04 06:42:47 +0900

    change: update environment.js [ci skip]

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -11,24 +11,24 @@ environment.plugins.prepend(
 )
 
 // https://wild-outdoorlife.com/ruby-on-rails/webpack5-error/
-const customConfig = {
-  resolve: {
-    fallback: {
-      dgram: false,
-      fs: false,
-      net: false,
-      tls: false,
-      child_process: false
-    }
-  }
-};
+// const customConfig = {
+//   resolve: {
+//     fallback: {
+//       dgram: false,
+//       fs: false,
+//       net: false,
+//       tls: false,
+//       child_process: false
+//     }
+//   }
+// };
 
-environment.config.delete('node.dgram')
-environment.config.delete('node.fs')
-environment.config.delete('node.net')
-environment.config.delete('node.tls')
-environment.config.delete('node.child_process')
+// environment.config.delete('node.dgram')
+// environment.config.delete('node.fs')
+// environment.config.delete('node.net')
+// environment.config.delete('node.tls')
+// environment.config.delete('node.child_process')
 
-environment.config.merge(customConfig);
+// environment.config.merge(customConfig);
 
 module.exports = environment
