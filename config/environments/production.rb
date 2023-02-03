5accb904a2ae9683c058ec61dbb954839af0b3c2
toshi-ue <t.ueno.dev+githubtoshiue@gmail.com>
2023-02-03 19:05:01 +0900

    update: production.rb [ci skip]

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,6 +30,9 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
+  # https://qiita.com/naota7118/items/72c4a6babb5d3b8d989c
+  config.assets.js_compressor = Uglifier.new(harmony: true)
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
   # config.asset_host = 'http://assets.example.com'
